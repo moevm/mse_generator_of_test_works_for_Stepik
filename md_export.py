1072411b7dd293177f75049aa4ede66173c5291b
AleksandrRybin <Vfibyrf007@gmail.com>
2018-10-24 19:51:11 +0300

    fix break line

--- md_export.py
+++ md_export.py
@@ -4,11 +4,11 @@ import download
 def process(course, test_name, var_qty=1, task_qty=10):
     file_name = test_name.replace(' ', '_') + '.md'
     with open(file_name, mode='w', encoding='utf8') as f:
-        f.write('# ' + test_name + '\n')
-        f.write('## Вариант 1\n')
-        f.write('**ФИО**___________________________________\n')
-        f.write('**Группа**_____\n')
-        f.write('**Дата**__________\n')
+        f.write('# ' + test_name + '  \n')
+        f.write('## Вариант 1  \n')
+        f.write('**ФИО**___________________________________  \n')
+        f.write('**Группа**_____  \n')
+        f.write('**Дата**__________  \n')
         for num, step in enumerate(course.get_chosen()):
             if step.get_type() == 'number':
                 number(f, step.get_path(), num + 1)
@@ -23,26 +23,25 @@ def choiсe(fp, step, num):
     src = open(step, mode='r')
     step = json.load(src)
     src.close()
-
-    fp.write(str(num) + '. ' + step['block']['text'] + '\n')
+    fp.write(str(num) + '. ' + step['block']['text'] + '  \n')
 
     for option in step['block']['source']['options']:
-        fp.write('\t' + '- ' + option['text'] + '\n')
+        fp.write('\t' + '- ' + option['text'] + '  \n')
 
 def number(fp, step, num):
     src = open(step, mode='r')
     step = json.load(src)
     src.close()
 
-    fp.write(str(num) + '. ' + step['block']['text'].replace('<p>', '').replace('</p>', '') + '\n')
-    fp.write('*(ответом является число)*\n')
-    fp.write('\t' + '- **Ответ:** _____________________\n')
+    fp.write(str(num) + '. ' + step['block']['text'] + '  \n')
+    fp.write('*(ответом является число)*  \n')
+    fp.write('\t' + '- **Ответ:** _____________________  \n')
 
 def string(fp, step, num):
     src = open(step, mode='r')
     step = json.load(src)
     src.close()
-
-    fp.write(str(num) + '. ' + step['block']['text'].replace('<p>', '').replace('</p>', '') + '\n')
-    fp.write('*(ответом является строка)*\n')
-    fp.write('\t' + '- **Ответ:** _____________________\n')
+    
+    fp.write(str(num) + '. ' + step['block']['text'] + '  \n')
+    fp.write('*(ответом является строка)*  \n')
+    fp.write('\t' + '- **Ответ:** _____________________  \n')
